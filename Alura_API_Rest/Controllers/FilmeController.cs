00b9bedfda98fcfa86c9d009f6fc02957f5f7ce4
Rodrigo Queriqueli <rdqueriqueli@gmail.com>
2021-12-29 10:02:56 -0300

    Add project files.

---
+++ Alura_API_Rest/Controllers/FilmeController.cs
@@ -0,0 +1,21 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")] //chamando o nome do controller (/filme, controller eh chamado)
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        /// <summary>
+        /// Cadastrar filme
+        /// </summary>
+        [HttpPost] //criando recurso novo no sistema
+        public void AdicionaFilme([FromBody] Filme filme)  //Filme que recebo vem do body da request
+        {
+            filmes.Add(filme);
+        }
+    }
+}
---
+++ Alura_API_Rest/Models/Filme.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        [Required (ErrorMessage = "The Title field is required.")] //campo requerido na request
+        public string Titulo { get; set; }
+        [Required (ErrorMessage = "The Director field is required.")]
+        public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "Genre must be shorter.")]
+        public string Genero { get; set; }
+        [Range(1,600, ErrorMessage= "The duration must be between 1 and 600 min")]
+        public int Duracao { get; set; }
+    }
+}
