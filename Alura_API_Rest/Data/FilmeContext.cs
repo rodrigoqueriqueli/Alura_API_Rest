011610fe44bf53247270c662d18c1f1dd0b6ff36
Rodrigo Queriqueli <rdqueriqueli@gmail.com>
2021-12-29 16:54:54 -0300

    Created FilmeContext and added ConnectionStrings.

---
+++ Alura_API_Rest/Data/FilmeContext.cs
@@ -0,0 +1,21 @@
+﻿using FilmesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesAPI.Data
+{
+    /// <summary>
+    /// Classe responsavel pela parte de contexto entre aplicacao (API) e o BD
+    /// </summary>
+    public class FilmeContext : DbContext
+    {
+        //Set de dados do BD
+        //Filme objeto q quero mapear e acessar no BD
+        public DbSet<Filme> Filmes { get; set; } 
+
+        //recebendo opcoes desse contexto
+        public FilmeContext(DbContextOptions<FilmeContext> dbContextOptions)  : base (dbContextOptions) //passando opcoes para o construtor do DbContext
+        {}
+
+
+    }
+}
---
+++ Alura_API_Rest/Startup.cs
@@ -0,0 +1,50 @@
+﻿using FilmesAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+namespace FilmesAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        //configurar service de acesso ao banco
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<FilmeContext>(optios => optios.UseMySQL(Configuration.GetConnectionString("FilmeConnection"))); //Add contexto de BD, passando options como parametro
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "FilmesAPI", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FilmesAPI v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
