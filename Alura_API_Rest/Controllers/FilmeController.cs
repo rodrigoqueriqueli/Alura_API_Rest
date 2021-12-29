17301ee3bacba37addc8a765b699b4c6213bea4d
Rodrigo Queriqueli <rdqueriqueli@gmail.com>
2021-12-29 15:47:22 -0300

    Changed responses for requests.

--- Alura_API_Rest/Controllers/FilmeController.cs
+++ Alura_API_Rest/Controllers/FilmeController.cs
@@ -14,16 +14,27 @@ namespace FilmesAPI.Controllers
         /// Cadastrar filme
         /// </summary>
         [HttpPost] //criando recurso novo no sistema
-        public void AdicionaFilme([FromBody] Filme filme)  //Filme que recebo vem do body da request
+        public IActionResult AdicionaFilme([FromBody] Filme filme)  //Filme que recebo vem do body da request
         {
             filme.Id = id++;
             filmes.Add(filme);
+            return CreatedAtAction(nameof(RecuperaFilmePorId), new { Id = filme.Id}, filme); //Retorna o status da requisicao e retorna em que lugar o recurso foi criado
         }
 
         [HttpGet] //recuperar recurso do sistema
-        public IEnumerable<Filme> RecuperarFilmes()
+        public IActionResult RecuperaFilmes()
         {
-            return filmes;
+            return Ok(filmes);
         } 
+
+        [HttpGet("{id}")] //indico que vou receber id por parametro (url da request)
+        public IActionResult RecuperaFilmePorId(int id)
+        {
+            var filme = filmes.FirstOrDefault(f => f.Id == id);
+            if (filme != null)
+                return Ok(filme); //retorna 200 Ok com o filme
+            else
+                return NotFound();
+        }
     }
 }
